428dfa96f02878ab32c01f0ea46ca6817097d76c
Adeyemi Temitope Adejare <adejareyemi1907@gmail.com>
2025-04-03 20:36:50 +0200

    gh

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Toggle hamburger menu
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    menu.classList.toggle("open");  // Toggle the "open" class on the menu
+    icon.classList.toggle("open");  // Toggle the "open" class on the icon
   }
+  
